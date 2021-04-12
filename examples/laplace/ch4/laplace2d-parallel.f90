b3d60f1373b97602d3145472a49a06201823a8f5
Jeff Larkin <jefflarkin@gmail.com>
2021-04-12 11:09:52 -0400

    Moved end data

--- examples/laplace/ch4/laplace2d-parallel.f90
+++ examples/laplace/ch4/laplace2d-parallel.f90
@@ -62,9 +62,9 @@ program laplace
     iter = iter + 1
 
   end do
+  !$acc end data
 
   call cpu_time(stop_time)
-  !$acc end data
   write(*,'(a,f10.3,a)')  ' completed in ', stop_time-start_time, ' seconds'
   deallocate (A,Anew)
 end program laplace
