ce24146ff04512d0d59cd671c7629d6ed4817026
Jeff Larkin <jlarkin@nvidia.com>
2024-01-04 13:02:23 -0500

    Update conf.py
    Removed recommonmark.

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ author = 'OpenACC.org'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+#    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
